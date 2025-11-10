098dfe238cce8e1af77d6a3add356e4defdba74b
JosipaI <ivezic.josipa@gmail.com>
2025-11-10 02:12:55 +0100

    fr

--- routes/fetch.routes.js
+++ routes/fetch.routes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Query = require('../models/QueryModel');
 
-router.use(express.json())
-// Sample route to handle the submitted data
 router.post('/', async (req, res) => {
-  let checked = req.body.checked
-  let result = await Query.fetchUserByQuery(req.body.field,req.body.checked)
-  //console.log(req.body.checked)
-  res.json({ message: result, check: checked});
+  try {
+
+    const result = await Query.fetchUserByQuery(req.body.field,req.body.checked);
+    return res.json({ message: result, check: req.body.checked });
+
+  } catch (err) {
+    console.error("Submit Data Error:", err);
+    return res.status(500).json({ error: "Internal server error", detail: err.message });
+  }
 });
 
-
 module.exports = router;
-
-
